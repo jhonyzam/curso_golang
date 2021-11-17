fce697ca1ff9fd0fbc75481bc4f72825a3765246
jhonyzam <jhonyzam90@gmail.com>
2021-11-17 11:42:05 -0300

    Class 4 50%

---
+++ class_4/loop_nomeado.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int
+
+loop:
+	for i = 0; i < 10; i++ {
+		fmt.Printf("for i = %d\n", i)
+
+		switch i {
+		case 2, 3:
+			fmt.Printf("Quebrando switch, i == %d. \n", i)
+			break
+		case 5:
+			fmt.Println("Quebrando loop, i == 5.")
+			break loop
+		}
+	}
+	fmt.Println("Fim.")
+}
